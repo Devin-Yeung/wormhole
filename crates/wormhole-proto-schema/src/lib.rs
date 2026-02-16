6569b960c55f515195b52ced566be8bed1985ffc
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-16 23:42:55 +0800

    feat(proto): add ShortCode type with Generated/Custom kinds

--- crates/wormhole-proto-schema/src/lib.rs
+++ crates/wormhole-proto-schema/src/lib.rs
@@ -1,4 +1,23 @@
+pub mod shortener {
+    pub mod v1 {
+        tonic::include_proto!("shortener.v1");
+    }
+}
+
+pub mod redirector {
+    pub mod v1 {
+        tonic::include_proto!("redirector.v1");
+    }
+}
+
+pub mod shortcode {
+    pub mod v1 {
+        tonic::include_proto!("shortcode.v1");
+    }
+}
+
 pub mod v1 {
-    tonic::include_proto!("shortener.v1");
-    tonic::include_proto!("redirector.v1");
+    pub use crate::redirector::v1::*;
+    pub use crate::shortcode::v1::*;
+    pub use crate::shortener::v1::*;
 }
