47696ade630e798f8dcb3823e6ed429a579b79ea
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-22 01:30:01 +0800

    refactor(state): use typed-builder for AppState

--- crates/wormhole-gateway/src/state.rs
+++ crates/wormhole-gateway/src/state.rs
@@ -1,18 +1,31 @@
 use std::sync::Arc;
-
+use typed_builder::TypedBuilder;
+use wormhole_redirector::redirector::Redirector;
 use wormhole_shortener::shortener::Shortener;
 
-#[derive(Clone)]
+#[derive(Clone, TypedBuilder)]
 pub struct AppState {
+    /// The shortener service used to create and manage short URLs.
+    #[builder(
+        setter(
+            fn transform<T: Shortener>(shortener: T) -> Arc<dyn Shortener> {
+                 Arc::new(shortener)
+            }
+        )
+    )]
     shortener: Arc<dyn Shortener>,
+    /// The redirector service used to resolve short URLs to their original URLs.
+    #[builder(
+        setter(
+            fn transform<T: Redirector>(redirector: T) -> Arc<dyn Redirector> {
+                 Arc::new(redirector)
+            }
+        )
+    )]
+    redirector: Arc<dyn Redirector>,
+    /// The base URL for public access to the short URLs.
+    #[builder]
     base_url: String,
 }
 
-impl AppState {
-    pub fn new(shortener: Arc<dyn Shortener>, public_base_url: impl Into<String>) -> Self {
-        Self {
-            shortener,
-            base_url: public_base_url.into(),
-        }
-    }
-}
+impl AppState {}
