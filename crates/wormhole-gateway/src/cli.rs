53f8066bb88135b59d4929655ce0932b12e83fe1
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-27 14:59:03 +0800

    chore(gateway): grpc service addr should not have default value

--- crates/wormhole-gateway/src/cli.rs
+++ crates/wormhole-gateway/src/cli.rs
@@ -7,8 +7,6 @@ pub const LISTEN_ADDR_ENV: &str = "WORMHOLE_GATEWAY_LISTEN_ADDR";
 pub const SHORTENER_ADDR_ENV: &str = "WORMHOLE_GATEWAY_SHORTENER_ADDR";
 pub const REDIRECTOR_ADDR_ENV: &str = "WORMHOLE_GATEWAY_REDIRECTOR_ADDR";
 pub const DEFAULT_LISTEN_ADDR: &str = "127.0.0.1:8080";
-pub const DEFAULT_SHORTENER_ADDR: &str = "http://127.0.0.1:50051";
-pub const DEFAULT_REDIRECTOR_ADDR: &str = "http://127.0.0.1:50052";
 
 #[derive(Debug, Parser)]
 #[command(name = "wormhole-gateway")]
@@ -18,11 +16,11 @@ pub struct CLI {
     /// Socket address to listen on, e.g., "127.0.0.1:8080"
     pub listen_addr: SocketAddr,
 
-    #[arg(long, env = SHORTENER_ADDR_ENV, default_value = DEFAULT_SHORTENER_ADDR)]
+    #[arg(long, env = SHORTENER_ADDR_ENV)]
     /// gRPC address for the shortener service, e.g., "http://127.0.0.1:50051"
     pub shortener_addr: String,
 
-    #[arg(long, env = REDIRECTOR_ADDR_ENV, default_value = DEFAULT_REDIRECTOR_ADDR)]
+    #[arg(long, env = REDIRECTOR_ADDR_ENV)]
     /// gRPC address for the redirector service, e.g., "http://127.0.0.1:50052"
     pub redirector_addr: String,
 }
