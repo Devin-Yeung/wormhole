69fd356c8354c7fe63822d142ac0252832afe3b1
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-03-01 00:50:16 +0800

    fix(proto-schema): fix compile error

--- crates/wormhole-proto-schema/build.rs
+++ crates/wormhole-proto-schema/build.rs
@@ -1,9 +1,22 @@
 use glob::glob;
+use std::error::Error;
+use std::path::PathBuf;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for entry in glob("../../proto/**/*.proto")? {
-        let path = entry?;
-        tonic_prost_build::compile_protos(path)?;
-    }
+fn main() -> Result<(), Box<dyn Error>> {
+    // collect all proto files
+    let protos = glob("../../proto/**/*.proto")?
+        .map(|entry| {
+            let path = entry?;
+            println!("cargo:rerun-if-changed={}", path.display());
+            Ok(path)
+        })
+        .collect::<Result<Vec<_>, Box<dyn Error>>>()?;
+
+    // get the include path
+    let proto_dir = PathBuf::from("../../proto");
+
+    // Compile all protos together so imports are resolved
+    tonic_prost_build::configure().compile_protos(&protos, &[proto_dir])?;
+
     Ok(())
 }
