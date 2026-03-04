2ad27eb39177876540ed0b3b0e64c71b53354755
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-03-04 15:09:30 +0800

    refactor(analytics): creating fk and along with creating table

--- analytics/sqlc/migrations/20260303115100_dim_visitors.sql
+++ analytics/sqlc/migrations/20260303115100_dim_visitors.sql
@@ -21,7 +21,18 @@ CREATE TABLE IF NOT EXISTS dim_visitors
     COMMENT = 'Visitor dimension: deduplicated profiles built from IP + User-Agent';
 -- +goose StatementEnd
 
+-- +goose StatementBegin
+ALTER TABLE fact_clicks
+    ADD CONSTRAINT fk_fact_clicks_visitor FOREIGN KEY (visitor_key) REFERENCES dim_visitors (visitor_key);
+-- +goose StatementEnd
+
 -- +goose Down
+
+-- +goose StatementBegin
+ALTER TABLE fact_clicks
+    DROP FOREIGN KEY fk_fact_clicks_visitor;
+-- +goose StatementEnd
+
 -- +goose StatementBegin
 DROP TABLE IF EXISTS dim_visitors;
 -- +goose StatementEnd
